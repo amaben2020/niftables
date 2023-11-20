2d89524ef8b235f8a448cc77da89541aa502a00d
algomachine007 <benneth@bamble.io>
2023-11-20 13:17:26 +0100

    creating text card component

--- src/components/elements/text-card/index.tsx
+++ src/components/elements/text-card/index.tsx
@@ -1,7 +1,9 @@
-import React from "react";
-
 const TextCard = () => {
-  return <div>TextCard</div>;
+  return (
+    <div className="border-main border pt-3 px-4 pb-5 rounded-md mb-3">
+      <p>Pre-launch investment opportunities for upcoming AI projects</p>
+    </div>
+  );
 };
 
 export default TextCard;
--- src/components/sections/two-columns/index.tsx
+++ src/components/sections/two-columns/index.tsx
@@ -1,3 +1,4 @@
+import TextCard from "@/components/elements/text-card";
 import VideoComponent from "@/components/elements/video";
 import SectionContainer from "@/components/layout/SectionContainer";
 import clsx from "clsx";
@@ -5,27 +6,22 @@ const TwoColumns = () => {
   return (
     <SectionContainer>
       <div className="grid grid-cols-4 gap-10">
-        <div className="col-span-2 border">
+        <div className="col-span-2">
           <h2 className={clsx("border-main", "pb-8")}>creon pass nft</h2>
-          <p className="gradient-text font-bold">
+          <p className="gradient-text font-bold pt-8 pb-10">
             The Creon NFT pass unlocks access to AI projects, the Creon
             launchpad, and a ticket to generate passive income through AI-driven
             tools
           </p>
-          <p>
-            The Creon NFT pass unlocks access to AI projects, the Creon
-            launchpad, and a ticket to generate passive income through AI-driven
-            tools
-          </p>{" "}
-          <p>
-            The Creon NFT pass unlocks access to AI projects, the Creon
-            launchpad, and a ticket to generate passive income through AI-driven
-            tools
-          </p>
+
+          <TextCard />
+          <TextCard />
+          <TextCard />
+          <TextCard />
           <button>BUT JSJS ASJ</button>
         </div>
 
-        <div className="col-span-2 border">
+        <div className="col-span-2">
           <VideoComponent url="/rotating-device.mp4" />
         </div>
       </div>
