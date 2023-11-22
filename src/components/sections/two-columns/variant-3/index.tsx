c91a8c55e92a379abfc916559f25a3e8aa8a0aae
algomachine007 <benneth@bamble.io>
2023-11-22 19:25:38 +0100

    improving accordion header ui

--- src/components/sections/two-columns/variant-3/index.tsx
+++ src/components/sections/two-columns/variant-3/index.tsx
@@ -9,35 +9,33 @@ const ColumnVariant3 = () => {
   return (
     <div className="grid grid-cols-2 relative gap-x-5 lg:gap-x-10 overflow-hidden">
       <div className=" col-span-2 lg:col-span-1 z-10">
-        {!isTablet ? (
-          <h3 className="font-primary text-4xl leading-[120%] uppercase z-10 break-words">
-            <span className="text-white">
-              Our vision is to support the innovation of AI blockchain projects
-            </span>{" "}
-            <span className="gradient-text">
-              while prioritizing communities and democratizing profits
-            </span>
+        <h3 className="font-primary text-[28px] lg:text-4xl leading-[120%] uppercase z-10 break-words block md:hidden xl:block">
+          <span className="text-white">
+            Our vision is to support the innovation of AI blockchain projects
+          </span>{" "}
+          <span className="gradient-text">
+            while prioritizing communities and democratizing profits
+          </span>
+        </h3>
+        <>
+          <h3 className="bg-gradient-to-r from-blue-500 to-purple-700 text-transparent bg-clip-text uppercase font-primary text-[28px] leading-[120%] hidden md:block xl:hidden">
+            Our mission is to provide unique, confidential AI-based tools, to
+            which you will gain lifetime access through NFT tokens and our CREON
+            token.
           </h3>
-        ) : (
-          <>
-            <h3 className="bg-gradient-to-r from-blue-500 to-purple-700 text-transparent bg-clip-text uppercase font-primary text-[28px] leading-[120%]">
-              our mission is to provide unique, confidential AI-based tools, to
-              which you will gain lifetime access through NFT tokens and our
-              CREON token.
-            </h3>
-            {isTablet && (
-              <div className="ml-auto w-full max-w-sm">
-                <Image
-                  src={IMAGES.alienTomb}
-                  width={436}
-                  height={500}
-                  alt=""
-                  className="w-full bg-gradient-to-r from-black via-black to-transparent"
-                />
-              </div>
-            )}
-          </>
-        )}
+
+          {isTablet && (
+            <div className="ml-auto w-full max-w-sm">
+              <Image
+                src={IMAGES.alienTomb}
+                width={436}
+                height={500}
+                alt=""
+                className="w-full bg-gradient-to-r from-black via-black to-transparent"
+              />
+            </div>
+          )}
+        </>
       </div>
 
       <div className="col-span-2 md:col-span-1 z-10 lg:justify-self-end">
