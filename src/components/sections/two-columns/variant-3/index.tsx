ad988449c73edba14371786f4778385e77c1c631
algomachine007 <benneth@bamble.io>
2023-11-22 19:29:02 +0100

    improving accordion header ui

--- src/components/sections/two-columns/variant-3/index.tsx
+++ src/components/sections/two-columns/variant-3/index.tsx
@@ -17,25 +17,24 @@ const ColumnVariant3 = () => {
             while prioritizing communities and democratizing profits
           </span>
         </h3>
-        <>
-          <h3 className="bg-gradient-to-r from-blue-500 to-purple-700 text-transparent bg-clip-text uppercase font-primary text-[28px] leading-[120%] hidden md:block xl:hidden">
-            Our mission is to provide unique, confidential AI-based tools, to
-            which you will gain lifetime access through NFT tokens and our CREON
-            token.
-          </h3>
 
-          {isTablet && (
-            <div className="ml-auto w-full max-w-sm">
-              <Image
-                src={IMAGES.alienTomb}
-                width={436}
-                height={500}
-                alt=""
-                className="w-full bg-gradient-to-r from-black via-black to-transparent"
-              />
-            </div>
-          )}
-        </>
+        <h3 className="bg-gradient-to-r from-blue-500 to-purple-700 text-transparent bg-clip-text uppercase font-primary text-[28px] leading-[120%] hidden md:block xl:hidden">
+          Our mission is to provide unique, confidential AI-based tools, to
+          which you will gain lifetime access through NFT tokens and our CREON
+          token.
+        </h3>
+
+        {isTablet && (
+          <div className="ml-auto w-full max-w-sm">
+            <Image
+              src={IMAGES.alienTomb}
+              width={436}
+              height={500}
+              alt=""
+              className="w-full bg-gradient-to-r from-black via-black to-transparent"
+            />
+          </div>
+        )}
       </div>
 
       <div className="col-span-2 md:col-span-1 z-10 lg:justify-self-end">
